90a56f7abcfbdf3dcc3f4eb83df73ec76b10e96b
Mohamed ElShaar <mamo.sha3ar@gmail.com>
2025-02-15 19:20:15 +0200

    - update MoveDetailsMapper to handle sorting actors and directors

--- app/src/main/java/com/company/moviesapp/presentation/mappers/MovieDetailsMapper.kt
+++ app/src/main/java/com/company/moviesapp/presentation/mappers/MovieDetailsMapper.kt
@@ -2,43 +2,83 @@ package com.company.moviesapp.presentation.mappers
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import com.company.moviesapp.data.remote.dto.Cast
+import com.company.moviesapp.data.remote.dto.Crew
 import com.company.moviesapp.data.remote.dto.MovieCreditsResponse
 import com.company.moviesapp.data.remote.dto.MovieDetailsResponse
-import com.company.moviesapp.data.remote.dto.SimilarMoviesResponse
 import com.company.moviesapp.presentation.models.CastDisplayModel
 import com.company.moviesapp.presentation.models.MovieDetailsDisplayModel
 import com.company.moviesapp.presentation.models.MovieDisplayModel
-import com.company.moviesapp.presentation.parseDate
 
 interface MovieDetailsMapper {
     fun mapMovieDetailsResponse(
         movieDetailsResponse: MovieDetailsResponse,
         movieCreditsResponse: MovieCreditsResponse,
-        similarMoviesResponse: SimilarMoviesResponse
+        similarMovies: List<MovieDisplayModel>,
+        similarMoviesCreditsResponse: List<MovieCreditsResponse>
     ): MovieDetailsDisplayModel
 }
 
 class MovieDetailsMapperImpl : MovieDetailsMapper {
+
     @RequiresApi(Build.VERSION_CODES.O)
     override fun mapMovieDetailsResponse(
         movieDetailsResponse: MovieDetailsResponse,
         movieCreditsResponse: MovieCreditsResponse,
-        similarMoviesResponse: SimilarMoviesResponse
+        similarMovies: List<MovieDisplayModel>,
+        similarMoviesCreditsResponse: List<MovieCreditsResponse>
     ): MovieDetailsDisplayModel {
-        val actors: List<CastDisplayModel> = emptyList()
-        val directors: List<CastDisplayModel> = emptyList()
+        val filteredActors: MutableList<Cast> = mutableListOf()
+        val filteredDirectors: MutableList<Crew> = mutableListOf()
 
-        val similarMovies: List<MovieDisplayModel> =
-            similarMoviesResponse.results.take(5).map { similarMovie ->
-                MovieDisplayModel(
-                    title = similarMovie.title,
-                    overview = similarMovie.overview,
-                    image = "https://image.tmdb.org/t/p/w300${similarMovie.posterPath}",
-                    addToWatch = false,
-                    releaseDate = parseDate(dateString = similarMovie.releaseDate)
+        for (movieCredits in similarMoviesCreditsResponse) {
+            // Get a list of Cast objects where knownForDepartment is "Acting"
+            filteredActors.addAll(movieCredits.cast.filter {
+                it.knownForDepartment.equals(
+                    "Acting",
+                    ignoreCase = true
                 )
-            }.toList()
+            })
 
+            // Get a list of Crew objects where knownForDepartment is "Directing"
+            filteredDirectors.addAll(movieCredits.crew.filter {
+                it.knownForDepartment.equals(
+                    "Directing",
+                    ignoreCase = true
+                )
+            })
+        }
+
+        var sortedActors: List<Cast> = filteredActors.sortedByDescending { it.popularity }
+        var sortedDirectors: List<Crew> = filteredDirectors.sortedByDescending { it.popularity }
+
+        sortedActors = sortedActors.take(5)
+        sortedDirectors = sortedDirectors.take(5)
+
+        val actors: MutableList<CastDisplayModel> = mutableListOf()
+        val directors: MutableList<CastDisplayModel> = mutableListOf()
+
+        for (actor in sortedActors) {
+            actors.add(
+                CastDisplayModel(
+                    name = actor.name,
+                    job = actor.knownForDepartment,
+                    popularity = actor.popularity,
+                    profilePath = if (actor.profilePath != null) "https://image.tmdb.org/t/p/w300${actor.profilePath}" else "",
+                )
+            )
+        }
+
+        for (director in sortedDirectors) {
+            directors.add(
+                CastDisplayModel(
+                    name = director.name,
+                    job = director.knownForDepartment,
+                    popularity = director.popularity,
+                    profilePath = if (director.profilePath != null) "https://image.tmdb.org/t/p/w300${director.profilePath}" else "",
+                )
+            )
+        }
 
         return MovieDetailsDisplayModel(
             title = movieDetailsResponse.title,
