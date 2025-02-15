04564da60afec88e1354b7fb8846d8ac0d06e2fc
Mohamed ElShaar <mamo.sha3ar@gmail.com>
2025-02-15 19:21:32 +0200

    - update MovieDetailsViewModel to depend on use case

--- app/src/main/java/com/company/moviesapp/presentation/viewmodel/MovieDetailsViewModel.kt
+++ app/src/main/java/com/company/moviesapp/presentation/viewmodel/MovieDetailsViewModel.kt
@@ -3,20 +3,14 @@ package com.company.moviesapp.presentation.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
-import com.company.moviesapp.data.remote.datasource.moviecredits.MovieCreditsRemoteDataSource
-import com.company.moviesapp.data.remote.datasource.moviedetails.MovieDetailsRemoteDataSource
-import com.company.moviesapp.data.remote.datasource.similarmovies.SimilarMoviesRemoteDataSource
-import com.company.moviesapp.presentation.mappers.MovieDetailsMapper
 import com.company.moviesapp.presentation.models.MovieDetailsDisplayModel
+import com.company.moviesapp.presentation.usecase.GetMovieDetailsScreenUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 class MovieDetailsViewModel(
-    private val movieDetailsRemoteDataSource: MovieDetailsRemoteDataSource,
-    private val movieCreditsRemoteDataSource: MovieCreditsRemoteDataSource,
-    private val similarMoviesRemoteDataSource: SimilarMoviesRemoteDataSource,
-    private val movieDetailsMapper: MovieDetailsMapper,
+    private val getMovieDetailsScreen: GetMovieDetailsScreenUseCase
 ) : ViewModel(), ViewModelProvider.Factory {
 
     private val _moviesState = MutableStateFlow<MovieDetailsUiState>(MovieDetailsUiState.Loading)
@@ -25,17 +19,8 @@ class MovieDetailsViewModel(
     suspend fun getMovieDetailsScreen(id: Int) {
         try {
             _moviesState.value = MovieDetailsUiState.Loading
-            val movieDetailsResponse = movieDetailsRemoteDataSource.getMovieDetails(id.toString())
-            val movieCreditsResponse = movieCreditsRemoteDataSource.getMovieCredits(id.toString())
-            val similarMoviesResponse =
-                similarMoviesRemoteDataSource.getSimilarMovies(id.toString())
-
-            val movieDetails: MovieDetailsDisplayModel = movieDetailsMapper.mapMovieDetailsResponse(
-                movieDetailsResponse,
-                movieCreditsResponse,
-                similarMoviesResponse
-            )
-
+            val movieDetails: MovieDetailsDisplayModel =
+                getMovieDetailsScreen.getMovieDetailsScreen(id.toString())
             _moviesState.value = MovieDetailsUiState.Success(movieDetails)
         } catch (e: Exception) {
             println(e.toString())
@@ -45,20 +30,14 @@ class MovieDetailsViewModel(
 }
 
 class MovieDetailsViewModelFactory(
-    private val movieDetailsRemoteDataSource: MovieDetailsRemoteDataSource,
-    private val movieCreditsRemoteDataSource: MovieCreditsRemoteDataSource,
-    private val similarMoviesRemoteDataSource: SimilarMoviesRemoteDataSource,
-    private val movieDetailsMapper: MovieDetailsMapper,
+    private val getMovieDetailsScreen: GetMovieDetailsScreenUseCase
 ) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
         if (modelClass.isAssignableFrom(MovieDetailsViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
             return MovieDetailsViewModel(
-                movieDetailsRemoteDataSource,
-                movieCreditsRemoteDataSource,
-                similarMoviesRemoteDataSource,
-                movieDetailsMapper,
+                getMovieDetailsScreen
             ) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
