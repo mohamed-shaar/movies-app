d4dc1568d301cd40bd1e76394b8a2eb3a09c5cd0
Mohamed ElShaar <mamo.sha3ar@gmail.com>
2025-02-16 02:37:54 +0200

    - add mockito dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,6 +83,7 @@ dependencies {
     kapt(libs.hilt.android.compiler)
 
     testImplementation(libs.junit)
+    testImplementation(libs.mockito.core)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
